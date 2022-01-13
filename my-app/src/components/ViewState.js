73ff4c635fc5bb48ccf31d3b80334c857d2434ec
Xinyi Wang <xinyiwang00@gmail.com>
2022-01-13 17:37:43 -0500

    viewstates loctaion file updated

--- my-app/src/components/ViewState.js
+++ my-app/src/components/ViewState.js
@@ -5,20 +5,361 @@ export const INITIAL_VIEW_STATE = {
     pitch: 0,
     bearing: 0
 };
-export const MD_ViewState = {
-    longitude: -77.04,
-    latitude: 38.76,
+export const AL_ViewState = {
+    longitude: -86.6807365,
+    latitude: 32.6010112,
     zoom: 6,
     pitch: 0,
     bearing: 0
-}
-export const WA_ViewState = {
-    longitude: -120.49232909712026,
-    latitude: 47.36041549207343,
-    zoom: 6.5,
+    } 
+    export const AK_ViewState = {
+    longitude: -158.7750198,
+    latitude: 61.3025006,
+    zoom: 6,
     pitch: 0,
     bearing: 0
-} 
-// export const VA_ViewState = { 
-
-// }
+    } 
+    export const AZ_ViewState = {
+    longitude: -111.930907,
+    latitude: 34.1682185,
+    zoom: 6,
+    pitch: 0,
+    bearing: 0
+    } 
+    export const AR_ViewState = {
+    longitude: -92.1313784,
+    latitude: 34.7519275,
+    zoom: 6,
+    pitch: 0,
+    bearing: 0
+    } 
+    export const CA_ViewState = {
+    longitude: -119.2704153,
+    latitude: 37.2718745,
+    zoom: 6,
+    pitch: 0,
+    bearing: 0
+    } 
+    export const CO_ViewState = {
+    longitude: -105.550567,
+    latitude: 38.9979339,
+    zoom: 6,
+    pitch: 0,
+    bearing: 0
+    } 
+    export const CT_ViewState = {
+    longitude: -72.757507,
+    latitude: 41.5187835,
+    zoom: 6,
+    pitch: 0,
+    bearing: 0
+    } 
+    export const DE_ViewState = {
+    longitude: -75.4189206,
+    latitude: 39.145251,
+    zoom: 6,
+    pitch: 0,
+    bearing: 0
+    } 
+    export const DC_ViewState = {
+    longitude: -77.0145666,
+    latitude: 38.8993487,
+    zoom: 6,
+    pitch: 0,
+    bearing: 0
+    } 
+    export const FL_ViewState = {
+    longitude: -83.8330166,
+    latitude: 27.9757279,
+    zoom: 6,
+    pitch: 0,
+    bearing: 0
+    } 
+    export const GA_ViewState = {
+    longitude: -83.2229757,
+    latitude: 32.6781248,
+    zoom: 6,
+    pitch: 0,
+    bearing: 0
+    } 
+    export const HI_ViewState = {
+    longitude: -157.505,
+    latitude: 20.46,
+    zoom: 6,
+    pitch: 0,
+    bearing: 0
+    } 
+    export const ID_ViewState = {
+    longitude: -114.1424303,
+    latitude: 45.4945756,
+    zoom: 6,
+    pitch: 0,
+    bearing: 0
+    } 
+    export const IL_ViewState = {
+    longitude: -89.504139,
+    latitude: 39.739318,
+    zoom: 6,
+    pitch: 0,
+    bearing: 0
+    } 
+    export const IN_ViewState = {
+    longitude: -86.441277,
+    latitude: 39.7662195,
+    zoom: 6,
+    pitch: 0,
+    bearing: 0
+    } 
+    export const IA_ViewState = {
+    longitude: -93.389798,
+    latitude: 41.9383166,
+    zoom: 6,
+    pitch: 0,
+    bearing: 0
+    } 
+    export const KS_ViewState = {
+    longitude: -98.3200779,
+    latitude: 38.4987789,
+    zoom: 6,
+    pitch: 0,
+    bearing: 0
+    } 
+    export const KY_ViewState = {
+    longitude: -85.7682399,
+    latitude: 37.8222935,
+    zoom: 6,
+    pitch: 0,
+    bearing: 0
+    } 
+    export const LA_ViewState = {
+    longitude: -91.4299097,
+    latitude: 30.9733766,
+    zoom: 6,
+    pitch: 0,
+    bearing: 0
+    } 
+    export const ME_ViewState = {
+    longitude: -69.0148656,
+    latitude: 45.2185133,
+    zoom: 6,
+    pitch: 0,
+    bearing: 0
+    } 
+    export const MD_ViewState = {
+    longitude: -77.2684162,
+    latitude: 38.8063524,
+    zoom: 6,
+    pitch: 0,
+    bearing: 0
+    } 
+    export const MA_ViewState = {
+    longitude: -71.718067,
+    latitude: 42.0629398,
+    zoom: 6,
+    pitch: 0,
+    bearing: 0
+    } 
+    export const MI_ViewState = {
+    longitude: -86.4158049,
+    latitude: 44.9435598,
+    zoom: 6,
+    pitch: 0,
+    bearing: 0
+    } 
+    export const MN_ViewState = {
+    longitude: -93.3655146,
+    latitude: 46.4418595,
+    zoom: 6,
+    pitch: 0,
+    bearing: 0
+    } 
+    export const MS_ViewState = {
+    longitude: -89.8772196,
+    latitude: 32.5851062,
+    zoom: 6,
+    pitch: 0,
+    bearing: 0
+    } 
+    export const MO_ViewState = {
+    longitude: -92.437099,
+    latitude: 38.3046615,
+    zoom: 6,
+    pitch: 0,
+    bearing: 0
+    } 
+    export const MT_ViewState = {
+    longitude: -110.044783,
+    latitude: 46.6797995,
+    zoom: 6,
+    pitch: 0,
+    bearing: 0
+    } 
+    export const NE_ViewState = {
+    longitude: -99.680902,
+    latitude: 41.5008195,
+    zoom: 6,
+    pitch: 0,
+    bearing: 0
+    } 
+    export const NV_ViewState = {
+    longitude: -117.0230604,
+    latitude: 38.502032,
+    zoom: 6,
+    pitch: 0,
+    bearing: 0
+    } 
+    export const NH_ViewState = {
+    longitude: -71.5799231,
+    latitude: 44.0012306,
+    zoom: 6,
+    pitch: 0,
+    bearing: 0
+    } 
+    export const NJ_ViewState = {
+    longitude: -74.7311156,
+    latitude: 40.1430058,
+    zoom: 6,
+    pitch: 0,
+    bearing: 0
+    } 
+    export const NM_ViewState = {
+    longitude: -106.0260685,
+    latitude: 34.1662325,
+    zoom: 6,
+    pitch: 0,
+    bearing: 0
+    } 
+    export const NY_ViewState = {
+    longitude: -73.97968,
+    latitude: 40.7056258,
+    zoom: 6,
+    pitch: 0,
+    bearing: 0
+    } 
+    export const NC_ViewState = {
+    longitude: -79.8912675,
+    latitude: 35.2145629,
+    zoom: 6,
+    pitch: 0,
+    bearing: 0
+    } 
+    export const ND_ViewState = {
+    longitude: -100.3022655,
+    latitude: 47.4678819,
+    zoom: 6,
+    pitch: 0,
+    bearing: 0
+    } 
+    export const OH_ViewState = {
+    longitude: -82.6692525,
+    latitude: 40.1903624,
+    zoom: 6,
+    pitch: 0,
+    bearing: 0
+    } 
+    export const OK_ViewState = {
+    longitude: -98.7165585,
+    latitude: 35.3097654,
+    zoom: 6,
+    pitch: 0,
+    bearing: 0
+    } 
+    export const OR_ViewState = {
+    longitude: -120.5380993,
+    latitude: 44.1419049,
+    zoom: 6,
+    pitch: 0,
+    bearing: 0
+    } 
+    export const PA_ViewState = {
+    longitude: -77.6046984,
+    latitude: 40.9945928,
+    zoom: 6,
+    pitch: 0,
+    bearing: 0
+    } 
+    export const RI_ViewState = {
+    longitude: -71.5064508,
+    latitude: 41.5827282,
+    zoom: 6,
+    pitch: 0,
+    bearing: 0
+    } 
+    export const SC_ViewState = {
+    longitude: -80.9470381,
+    latitude: 33.62505,
+    zoom: 6,
+    pitch: 0,
+    bearing: 0
+    } 
+    export const SD_ViewState = {
+    longitude: -100.2471641,
+    latitude: 44.2126995,
+    zoom: 6,
+    pitch: 0,
+    bearing: 0
+    } 
+    export const TN_ViewState = {
+    longitude: -85.9785989,
+    latitude: 35.830521,
+    zoom: 6,
+    pitch: 0,
+    bearing: 0
+    } 
+    export const TX_ViewState = {
+    longitude: -100.0768425,
+    latitude: 31.1693363,
+    zoom: 6,
+    pitch: 0,
+    bearing: 0
+    } 
+    export const UT_ViewState = {
+    longitude: -111.547028,
+    latitude: 39.4997605,
+    zoom: 6,
+    pitch: 0,
+    bearing: 0
+    } 
+    export const VT_ViewState = {
+    longitude: -72.4477828,
+    latitude: 43.8717545,
+    zoom: 6,
+    pitch: 0,
+    bearing: 0
+    } 
+    export const VA_ViewState = {
+    longitude: -79.4587861,
+    latitude: 38.0033855,
+    zoom: 6,
+    pitch: 0,
+    bearing: 0
+    } 
+    export const WA_ViewState = {
+    longitude: -77.0145665,
+    latitude: 38.8993487,
+    zoom: 6,
+    pitch: 0,
+    bearing: 0
+    } 
+    export const WV_ViewState = {
+    longitude: -80.1816905,
+    latitude: 38.9201705,
+    zoom: 6,
+    pitch: 0,
+    bearing: 0
+    } 
+    export const WI_ViewState = {
+    longitude: -89.8267049,
+    latitude: 44.7862968,
+    zoom: 6,
+    pitch: 0,
+    bearing: 0
+    } 
+    export const WY_ViewState = {
+    longitude: -107.5545669,
+    latitude: 43.000325,
+    zoom: 6,
+    pitch: 0,
+    bearing: 0
+    } 
+    
