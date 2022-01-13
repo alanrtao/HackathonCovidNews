5f09011c65289f8a2aaaf93b1568afe3a1776422
Xinyi Wang <xinyiwang00@gmail.com>
2022-01-13 14:33:10 -0500

    modified hover over color

--- my-app/src/components/Newsformat.js
+++ my-app/src/components/Newsformat.js
@@ -5,19 +5,18 @@ function Newsformat(props) {
     return (
         <Box
         sx={{
-            width: 300,
+            width: '90%',
             height: 200,
             border: 1,
-            borderRadius: 5,
+            borderRadius: 1,
             borderColor: 'grey.500',
-            backgroundColor: '#FFFFE0',
+            backgroundColor: '#EBEBEB',
             '&:hover': {
-              backgroundColor: '#FFF8DC',
-              opacity: [0.3, 0.9, 0.2],
+              backgroundColor: '#F1F1F1',
             },
           }}
         >
-            <h1>   Title: </h1>
+            <h3>   Title: </h3>
             <h3>   Date: </h3>
             <h4>   Link: <a href="https://www.google.com/" target="_blank">Visit site</a> </h4>
 
